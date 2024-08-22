259604d64b9a90e606ef533913a0131639cf3e36
ricky <richardhull11@gmail.com>
2024-08-22 23:44:07 +0100

    - added new GameMap and WorldMap that inherit from UAegisMap

---
+++ Source/Aegis/Map/AegisWorldMap.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AegisMap.h"
+#include "AegisWorldMap.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AEGIS_API UAegisWorldMap : public UAegisMap
+{
+	GENERATED_BODY()
+};
