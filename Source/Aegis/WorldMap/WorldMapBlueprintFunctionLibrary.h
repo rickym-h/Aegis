f6553a83824c109294df8da9d217ccb6ac429198
ricky <richardhull11@gmail.com>
2024-08-28 02:11:51 +0100

    - implemented a basic map selection screen with a static map in the world selector

---
+++ Source/Aegis/WorldMap/WorldMapBlueprintFunctionLibrary.h
@@ -0,0 +1,109 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "WorldMapBlueprintFunctionLibrary.generated.h"
+
+UENUM(BlueprintType)
+enum EMapNodeType
+{
+	MinorEnemy,
+	EliteEnemy,
+	RestSite,
+	Shop,
+	BossEnemy,
+};
+
+USTRUCT(BlueprintType)
+struct FMapNodeCoordinate
+{
+	GENERATED_BODY()
+
+	FMapNodeCoordinate() {}
+
+	FMapNodeCoordinate(const int32 Layer, const int32 Row)
+	{
+		this->Layer = Layer;
+		this->Row = Row;
+	}
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int32 Layer = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int32 Row = 0;
+};
+
+
+USTRUCT(BlueprintType)
+struct FMapNode
+{
+	GENERATED_BODY()
+
+	FMapNode() {}
+
+	FMapNode(const EMapNodeType InNodeType, const FMapNodeCoordinate InCoordinate)
+	{
+		this->NodeType = InNodeType;
+		this->NodeCoordinate = InCoordinate;
+	}
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TEnumAsByte<EMapNodeType> NodeType = MinorEnemy;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TArray<FMapNodeCoordinate> OutGoingConnections = TArray<FMapNodeCoordinate>();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FMapNodeCoordinate NodeCoordinate = FMapNodeCoordinate();
+	FVector2D GetCanvasLocation() const
+	{
+		return FVector2D(NodeCoordinate.Layer, NodeCoordinate.Row);
+	}
+};
+
+USTRUCT(BlueprintType)
+struct FWorldMapData
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TArray<FMapNode> MapNodes;
+};
+
+
+USTRUCT(BlueprintType)
+struct FWorldMapCreationConfig
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int32 StartingLayerNodesCount;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int32 PreBossLayerNodesCount;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int32 LayersCount;
+
+	int32 GetGridHeight() const
+	{
+		return FMath::Max(StartingLayerNodesCount, PreBossLayerNodesCount);
+	}
+};
+
+
+/**
+ * 
+ */
+UCLASS()
+class AEGIS_API UWorldMapBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+	static FWorldMapData GenerateWorldMapData(const FWorldMapCreationConfig MapConfig);
+	
+};
