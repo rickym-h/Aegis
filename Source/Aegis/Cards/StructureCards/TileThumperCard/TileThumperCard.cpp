cf8782b4709288c2717fb7962253b0d48847ba21
ricky <richardhull11@gmail.com>
2024-11-02 01:11:10 +0000

    - added a TileThumperCard that should spawn an initiate a thumper when it is played

---
+++ Source/Aegis/Cards/StructureCards/TileThumperCard/TileThumperCard.cpp
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TileThumperCard.h"
+
+#include "Aegis/Core/GameStates/AegisGameStateBase.h"
+#include "Aegis/Map/AegisGameMap.h"
+#include "Aegis/Structures/Towers/TileThumper/TileThumper.h"
+#include "Kismet/GameplayStatics.h"
+
+bool UTileThumperCard::PlayCard_Implementation(const FTileCoord& LocationCoord)
+{
+	const AAegisGameStateBase* GameState = Cast<AAegisGameStateBase>(UGameplayStatics::GetGameState(GetWorld()));
+	
+	// Get the map, and attempt to build an instance of the archer tower on the map
+	AStructure* Structure = GameState->AegisMap->AddStructureToMap(this, LocationCoord);
+	if (!Structure)
+	{
+		UE_LOG(LogTemp, Error, TEXT("UTileThumperCard::PlayCard_Implementation - Failed to add structure to map!"))
+		return false;
+	}
+	UE_LOG(LogTemp, Display, TEXT("UTileThumperCard::PlayCard_Implementation - Successfully added structure to map!"))
+
+	// Set structure data (e.g. tower stats)
+	if (ATileThumper* TileThumper = Cast<ATileThumper>(Structure))
+	{
+		TileThumper->InitTileThumper(AttackSpeed, TileRangeOffsets);
+	} else
+	{
+		UE_LOG(LogTemp, Error, TEXT("UTileThumperCard::PlayCard_Implementation - Structure not TileThumper tower! Failed to InitProjectileTower()"))
+	}
+	
+	UGameplayStatics::FinishSpawningActor(Structure, Structure->ActorTransform);
+	UE_LOG(LogTemp, Display, TEXT("UTileThumperCard::PlayCard_Implementation - Finished spawning structure!"))
+	
+	// If successful, return true
+	return true;
+}
+
+TSet<FTileCoord> UTileThumperCard::GetRangeTileOffsets_Implementation()
+{
+	return TileRangeOffsets;
+}
---
+++ Source/Aegis/Cards/StructureCards/TileThumperCard/TileThumperCard.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Aegis/Cards/StructureCard.h"
+#include "Aegis/Cards/Interfaces/PlayableCardInterface.h"
+#include "Aegis/Cards/Interfaces/TileRangeInterface.h"
+#include "TileThumperCard.generated.h"
+
+/**
+ * 
+ */
+UCLASS(HideDropDown)
+class AEGIS_API UTileThumperCard : public UStructureCard, public IPlayableCardInterface, public ITileRangeInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Tower Card Data")
+	float AttackSpeed = 0.f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Tower Card Data")
+	TSet<FTileCoord> TileRangeOffsets = TSet<FTileCoord>();
+	
+
+protected:
+	virtual bool PlayCard_Implementation(const FTileCoord& LocationCoord) override;
+	virtual TSet<FTileCoord> GetRangeTileOffsets_Implementation() override;
+};
